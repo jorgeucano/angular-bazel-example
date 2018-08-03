88bd6fd95354ba40608003811cf62e72953ae695
Greg Magolan <gmagolan@gmail.com>
2018-08-03 10:58:23 -0700

    Devmode working again with no code splitting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,21 +1,9 @@
 
 import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {RouterModule, Routes} from '@angular/router';
-import {StoreModule} from '@ngrx/store';
+import {RouterModule} from '@angular/router';
 
 import {AppComponent} from './app.component';
-import {todoReducer} from './reducers/reducers';
 
-export const appRoutes: Routes = [
-  {path: '', pathMatch: 'full', loadChildren: './hello-world/hello-world.module#HelloWorldModule'},
-  {path: 'todos', pathMatch: 'full', loadChildren: './todos/todos.module#TodosModule'},
-];
-
-@NgModule({
-  imports: [BrowserModule, RouterModule.forRoot(appRoutes), StoreModule.forRoot({todoReducer})],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
-})
+@NgModule({declarations: [AppComponent], imports: [RouterModule], exports: [AppComponent]})
 export class AppModule {
 }
